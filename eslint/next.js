55f9871abaa870aa97e69fa87ec133066d52fb46
John Pham <johnphammail@gmail.com>
2022-04-05 15:03:24 -0700

    feat(eslint): add eslint rule override to allow for <Link>s with child <a>s

--- eslint/next.js
+++ eslint/next.js
@@ -20,6 +20,18 @@ module.exports = {
     {
       files: JAVASCRIPT_FILES,
       parserOptions: { babelOptions },
+      rules: {
+        // Ignores rule violation for anchors that are children of <Link>s.
+        // See https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402#issuecomment-368305051
+        'jsx-a11y/anchor-is-valid': [
+          'error',
+          {
+            components: ['Link'],
+            specialLink: ['hrefLeft', 'hrefRight'],
+            aspects: ['invalidHref', 'preferButton'],
+          },
+        ],
+      },
     },
   ],
 };
