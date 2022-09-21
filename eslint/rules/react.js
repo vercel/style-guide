f2cfc320fa22db7c353b7451695ac5c9d310de87
Brody McKee <mrmckeb@gmail.com>
2022-09-21 18:05:16 +1000

    feat(eslint): enable `react/jsx-no-leaked-render`

--- eslint/rules/react.js
+++ eslint/rules/react.js
@@ -16,40 +16,46 @@ module.exports = {
     /**
      * Require an explicit type when using button elements.
      *
-     * 🚫 Not fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/button-has-type.md
+     * 🚫 Not fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/button-has-type.md
      */
     'react/button-has-type': 'warn',
     /**
      * Require consistent function type for function components.
      *
-     * 🔧 Fixable - https://github.com/yannickcr/eslint-plugin-react/blob/HEAD/docs/rules/function-component-definition.md
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/HEAD/docs/rules/function-component-definition.md
      */
     'react/function-component-definition': 'warn',
     /**
      * Require consistent boolean attributes notation in JSX.
      *
-     * 🔧 Fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
      */
     'react/jsx-boolean-value': 'warn',
     /**
      * Disallow unnecessary curly braces in JSX props and children.
      *
-     * 🔧 Fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-curly-brace-presence.md
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-curly-brace-presence.md
      */
     'react/jsx-curly-brace-presence': 'warn',
     /**
      * Require using shorthand form for React fragments, unless required.
      *
-     * 🔧 Fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-fragments.md
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-fragments.md
      */
     'react/jsx-fragments': 'warn',
+    /**
+     * Prevent problematic leaked values from being rendered.
+     *
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-leaked-render.md
+     */
+    'react/jsx-no-leaked-render': 'warn',
     /**
      * Prevents usage of unsafe `target='_blank'`.
      *
      * This rule is a part of `react/recommended`, but we've modified it to
      * allow referrer.
      *
-     * 🔧 Fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-target-blank.md
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-target-blank.md
      */
     'react/jsx-no-target-blank': [
       'error',
@@ -60,31 +66,31 @@ module.exports = {
     /**
      * Disallow empty React fragments.
      *
-     * 🔧 Fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-useless-fragment.md
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-no-useless-fragment.md
      */
     'react/jsx-no-useless-fragment': 'warn',
     /**
      * Require the use of PascalCase for user-defined JSX components.
      *
-     * 🚫 Not fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
+     * 🚫 Not fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
      */
     'react/jsx-pascal-case': 'warn',
     /**
      * Require props to be sorted alphabetically.
      *
-     * 🔧 Fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/jsx-sort-props.md
      */
     'react/jsx-sort-props': 'warn',
     /**
      * Disallow usage of Array index in keys.
      *
-     * 🚫 Not fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-array-index-key.md
+     * 🚫 Not fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/no-array-index-key.md
      */
     'react/no-array-index-key': 'warn',
     /**
      * Disallow closing tags for components without children.
      *
-     * 🔧 Fixable - https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
+     * 🔧 Fixable - https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
      */
     'react/self-closing-comp': 'warn',
   },
