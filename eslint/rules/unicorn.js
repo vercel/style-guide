ddb49feb93eb06acb7ca43f9015319474a7d4595
Brody McKee <mrmckeb@gmail.com>
2022-09-21 18:59:04 +1000

    feat(eslint): enable `unicorn/prefer-node-protocol`
    Resolves #36

--- eslint/rules/unicorn.js
+++ eslint/rules/unicorn.js
@@ -12,5 +12,11 @@ module.exports = {
         case: 'kebabCase',
       },
     ],
+    /**
+     * Require using the `node:` protocol when importing Node.js built-in modules.
+     *
+     * 🔧 Fixable - https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-node-protocol.md
+     */
+    'unicorn/prefer-node-protocol': 'warn',
   },
 };
