bb1ec8bcf5bc02d92d8300ad6bae95d4eb072f02
Cody Brouwers <CodyBrouwers@users.noreply.github.com>
2022-09-22 12:18:07 +0900

    fix(eslint): add playwright namespace to rules

--- eslint/rules/playwright-test.js
+++ eslint/rules/playwright-test.js
@@ -5,18 +5,18 @@ module.exports = {
      *
      * 🔧 Fixable - https://github.com/playwright-community/eslint-plugin-playwright/blob/main/docs/rules/prefer-lowercase-title.md
      */
-    'prefer-lowercase-title': 'warn',
+    'playwright/prefer-lowercase-title': 'warn',
     /**
      * Require using `toHaveLength` over explicitly checking lengths.
      *
      * 🔧 Fixable - https://github.com/playwright-community/eslint-plugin-playwright/blob/main/docs/rules/prefer-lowercase-title.md
      */
-    'prefer-to-have-length': 'warn',
+    'playwright/prefer-to-have-length': 'warn',
     /**
      * Require test cases and hooks to be inside a `test.describe` block.
      *
      * 🚫 Not fixable - https://github.com/playwright-community/eslint-plugin-playwright/blob/main/docs/rules/prefer-lowercase-title.md
      */
-    'require-top-level-describe': 'error',
+    'playwright/require-top-level-describe': 'error',
   },
 };
