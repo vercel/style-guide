dd7004d580d706b7e54e65dbd7497e06416dc508
Brody McKee <mrmckeb@users.noreply.github.com>
2024-03-05 12:03:06 +1100

    feat(deps): update `@typescript-eslint/*` dependencies (#102)
    BREAKING CHANGE: Contains changes to `@typescript-eslint/*` rules and configuration.

--- eslint/typescript.js
+++ eslint/typescript.js
@@ -8,18 +8,14 @@ module.exports = {
     {
       files: TYPESCRIPT_FILES,
       extends: [
-        'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-type-checked',
-        'plugin:@typescript-eslint/strict',
         'plugin:@typescript-eslint/strict-type-checked',
-        'plugin:@typescript-eslint/stylistic',
         'plugin:@typescript-eslint/stylistic-type-checked',
         'plugin:import/typescript',
         'prettier',
         require.resolve('./rules/typescript'),
         require.resolve('./rules/typescript/extension'),
         require.resolve('./rules/typescript/import'),
-        require.resolve('./rules/typescript/strict'),
         require.resolve('./rules/tsdoc'),
       ],
     },
